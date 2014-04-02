0e8ba94fc24a7bc3e778d966229b96c5c9c362ab
Aemmel <emilkasper@web.de>
2014-04-02 22:56:01 +0200

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,22 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+static const int WIDTH = 30;
+static const int HEIGHT = 20;
+
+class Player
+{
+public:
+	int get_X_Cor();
+	int get_Y_Cor();
+	void set_X_Cor(int newXPos);
+	void set_Y_Cor(int newYPos);
+	void set_Cor(int newXPos, int newYPos);
+	
+private:
+	int _PosX;
+	int _PosY;
+
+};
+
+#endif
